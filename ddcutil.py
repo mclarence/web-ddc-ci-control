31c4704da99ad99c6941ff38647f6176606ea02b
Clarence Mesina <public@cm0.dev>
2023-11-23 19:06:41 +1100

    fix concat issue

--- ddcutil.py
+++ ddcutil.py
@@ -3,9 +3,9 @@ import subprocess
 ddcutil_path = "/usr/bin/ddcutil"
 
 def set_vcp(i2c_bus: int, vcp: int, value: int):
-    return os.system(ddcutil_path + " --bus " + i2c_bus + " setvcp " + vcp + " " + value)
+    return os.system(ddcutil_path + " --bus " + str(i2c_bus) + " setvcp " + str(vcp) + " " + str(value))
 
 def get_vcp(i2c_bus: int, vcp: int):
     # > ddcutil getvcp 0x10 --terse
     # VCP 10 C 0 100
-    return subprocess.check_output(ddcutil_path + " --bus " + i2c_bus + " getvcp " + vcp + " --terse", shell=True).decode("utf-8").split(" ")[3]
+    return subprocess.check_output(ddcutil_path + " --bus " + str(i2c_bus) + " getvcp " + str(vcp) + " --terse", shell=True).decode("utf-8").split(" ")[3]
